77841c8d23dfea8b33f453cf4dd7fb16249ac87e
Demir Arikan <dmrarikan@gmail.com>
2024-06-22 19:26:51 -0400

    created seg model class with utility funcs

--- depth_control.py
+++ depth_control.py
@@ -5,26 +5,15 @@ import torch
 from torchvision import transforms
 import monai
 import numpy as np
+from needle_seg_model import NeedleSegModel
 
-target_depth = 0.5
-current_depth = 0.0
+target_depth_relative = 0.5
+current_depth_relative = 0.0
 
 n_bscans = 5
 dims = (0.1, 5)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = monai.networks.nets.UNet(
-    spatial_dims=2,
-    in_channels=1,
-    out_channels=4,
-    channels=(16, 32, 64, 128, 256, 512),
-    strides=(2, 2, 2, 2, 2),
-    kernel_size=3,
-).to(device)
-
-model.load_state_dict(torch.load('weights/straight_needle_seg_model.pth'))
-model.eval()
-print("Model loaded")
+seg_model = NeedleSegModel('weights/straight_needle_seg_model.pth')
 
 # leica_reader = leica_engine.LeicaEngine(ip_address="192.168.1.75",   
 #                                n_bscans=n_bscans, 
@@ -37,22 +26,12 @@ leica_reader = MockLeica('/home/demir/Desktop/jhu_project/oct_scans/jun11/2')
 print("Leica reader initialized")
 
 if __name__ == '__main__':
-    while current_depth < target_depth:
+    while current_depth_relative < target_depth_relative:
         oct_volume = leica_reader.__get_b_scans_volume__()
 
-        oct_volume = oct_volume.transpose(1, 0, 2)
-        oct_volume = np.rot90(oct_volume, axes=(1, 2))
-        oct_volume = oct_volume.astype(np.float32)
-        print(oct_volume.shape)
+        oct_volume = seg_model.prepare_vol_from_leica_engine(oct_volume)
 
-        oct_volume = torch.tensor(oct_volume).to(device)
-        oct_volume = transforms.Pad((12, 0, 12, 0))(oct_volume)
-        oct_volume = oct_volume.unsqueeze(1)
-
-        with torch.no_grad():
-            seg_volume = model(oct_volume)
-            seg_volume = torch.argmax(torch.softmax(seg_volume, dim=1), dim=1)
-            seg_volume = seg_volume.cpu().numpy()
+        seg_volume = seg_model.segment_volume(oct_volume)
 
         needle_point_cloud = oct_point_cloud.create_point_cloud_from_vol(seg_volume, seg_index=[1])
         needle_tip_coords = oct_point_cloud.needle_cloud_find_needle_tip(needle_point_cloud, 
@@ -69,10 +48,10 @@ if __name__ == '__main__':
         rpe_tip_coords = rpe_points[(rpe_points[:, 0] == needle_tip_coords[0]) & 
                                     (rpe_points[:, 2] == needle_tip_coords[2])]
         
-        _, _, current_depth = oct_point_cloud.calculate_needle_tip_depth(needle_tip_coords, 
+        _, _, current_depth_relative = oct_point_cloud.calculate_needle_tip_depth(needle_tip_coords, 
                                                                         ilm_tip_coords[0], 
                                                                         rpe_tip_coords[0])
-        print(f"Current depth: {current_depth}")
+        print(f"Current depth: {current_depth_relative}")
         
     print("Reached target depth")
 
---
+++ needle_seg_model.py
@@ -0,0 +1,36 @@
+import torch
+from torchvision import transforms
+import monai
+import numpy as np
+
+class NeedleSegModel():
+    def __init__(self, weights_path):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = monai.networks.nets.UNet(
+            spatial_dims=2,
+            in_channels=1,
+            out_channels=4,
+            channels=(16, 32, 64, 128, 256, 512),
+            strides=(2, 2, 2, 2, 2),
+            kernel_size=3,
+        ).to(self.device)
+
+        self.model.load_state_dict(torch.load(weights_path))
+        self.model.eval()
+        print('Model loaded successfully')
+
+    def prepare_vol_from_leica_engine(self, oct_volume):
+        oct_volume = oct_volume.transpose(1, 0, 2)
+        oct_volume = np.rot90(oct_volume, axes=(1, 2))
+        oct_volume = oct_volume.astype(np.float32)
+        oct_volume = torch.tensor(oct_volume).to(self.device)
+        oct_volume = transforms.Pad((12, 0, 12, 0))(oct_volume)
+        oct_volume = oct_volume.unsqueeze(1)
+        return oct_volume
+    
+    def segment_volume(self, oct_volume):
+        with torch.no_grad():
+            seg_volume = self.model(oct_volume)
+            seg_volume = torch.argmax(torch.softmax(seg_volume, dim=1), dim=1)
+            seg_volume = seg_volume.cpu().numpy()
+        return seg_volume

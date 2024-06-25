f7d1c91eee690d57652150c1e215c37f46359e0d
demir arikan <dmrarikan@gmail.com>
2024-06-24 21:58:06 -0400

    added robot controller

--- depth_control.py
+++ depth_control.py
@@ -2,6 +2,8 @@
 import oct_point_cloud
 from mock_leica import MockLeica
 from needle_seg_model import NeedleSegModel
+import rospy
+from robot_controller import RobotController
 
 target_depth_relative = 0.5
 current_depth_relative = 0.0
@@ -12,54 +14,60 @@ dims = (0.1, 5)
 save_point_cloud_image = True
 image_count = 0
 
-seg_model = NeedleSegModel('weights/straight_needle_seg_model.pth')
-
-# leica_reader = leica_engine.LeicaEngine(ip_address="192.168.1.75",   
-#                                n_bscans=n_bscans, 
-#                                xd=dims[0], 
-#                                yd=dims[1], 
-#                                zd=3.379,
-#                                )
-
-leica_reader = MockLeica('/home/demir/Desktop/jhu_project/oct_scans/jun11/2')
-print("Leica reader initialized")
-
 if __name__ == '__main__':
+    rospy.init_node('depth_controller', anonymous=True)
+
+    robot_controller = RobotController()
+    seg_model = NeedleSegModel('weights/straight_needle_seg_model.pth')
+    # leica_reader = leica_engine.LeicaEngine(ip_address="192.168.1.75",   
+    #                                n_bscans=n_bscans, 
+    #                                xd=dims[0], 
+    #                                yd=dims[1], 
+    #                                zd=3.379,
+    #                                )
+    leica_reader = MockLeica('/home/peiyao/Desktop/Demir/oct_volumes/jun11/2')
+    print("Leica reader initialized")
+
     while current_depth_relative < target_depth_relative:
-        oct_volume = leica_reader.__get_b_scans_volume__()
+    # while True:
+        try:
+            oct_volume = leica_reader.__get_b_scans_volume__()
 
-        oct_volume = seg_model.prepare_vol_from_leica_engine(oct_volume)
+            oct_volume = seg_model.prepare_vol_from_leica_engine(oct_volume)
 
-        seg_volume = seg_model.segment_volume(oct_volume)
+            seg_volume = seg_model.segment_volume(oct_volume, debug=True)
 
-        needle_point_cloud = oct_point_cloud.create_point_cloud_from_vol(seg_volume, seg_index=[1])
-        needle_tip_coords, clean_needle_point_cloud = oct_point_cloud.needle_cloud_find_needle_tip(needle_point_cloud, 
-                                                                        return_clean_point_cloud=True)
-        
-        ilm_depth_map = oct_point_cloud.get_depth_map(seg_volume, seg_index=2)
-        rpe_depth_map = oct_point_cloud.get_depth_map(seg_volume, seg_index=3)
+            needle_point_cloud = oct_point_cloud.create_point_cloud_from_vol(seg_volume, seg_index=[1])
+            needle_tip_coords, clean_needle_point_cloud = oct_point_cloud.needle_cloud_find_needle_tip(needle_point_cloud, 
+                                                                            return_clean_point_cloud=True)
+            
+            ilm_depth_map = oct_point_cloud.get_depth_map(seg_volume, seg_index=2)
+            rpe_depth_map = oct_point_cloud.get_depth_map(seg_volume, seg_index=3)
 
-        ilm_points, rpe_points = oct_point_cloud.inpaint_layers(ilm_depth_map, rpe_depth_map)
+            ilm_points, rpe_points = oct_point_cloud.inpaint_layers(ilm_depth_map, rpe_depth_map)
 
-        ilm_tip_coords = ilm_points[(ilm_points[:, 0] == needle_tip_coords[0]) & 
-                                    (ilm_points[:, 2] == needle_tip_coords[2])]
-        
-        rpe_tip_coords = rpe_points[(rpe_points[:, 0] == needle_tip_coords[0]) & 
-                                    (rpe_points[:, 2] == needle_tip_coords[2])]
-        
-        _, _, current_depth_relative = oct_point_cloud.calculate_needle_tip_depth(needle_tip_coords, 
-                                                                        ilm_tip_coords[0], 
-                                                                        rpe_tip_coords[0])
-        print(f"Current depth: {current_depth_relative}")
+            ilm_tip_coords = ilm_points[(ilm_points[:, 0] == needle_tip_coords[0]) & 
+                                        (ilm_points[:, 2] == needle_tip_coords[2])]
+            
+            rpe_tip_coords = rpe_points[(rpe_points[:, 0] == needle_tip_coords[0]) & 
+                                        (rpe_points[:, 2] == needle_tip_coords[2])]
+            
+            _, _, current_depth_relative = oct_point_cloud.calculate_needle_tip_depth(needle_tip_coords, 
+                                                                            ilm_tip_coords[0], 
+                                                                            rpe_tip_coords[0])
+            print(f"Current depth: {current_depth_relative}")
 
-        if save_point_cloud_image:
-            oct_point_cloud.create_save_point_cloud(clean_needle_point_cloud,
-                                                    ilm_points,
-                                                    rpe_points,
-                                                    needle_tip_coords,
-                                                    save_name=f'needle_tip_{image_count}_{current_depth_relative:.2f}')
-            image_count += 1
+            robot_controller.move_forward_needle_axis(duration_sec=0.5)
 
+            if save_point_cloud_image:
+                oct_point_cloud.create_save_point_cloud(clean_needle_point_cloud,
+                                                        ilm_points,
+                                                        rpe_points,
+                                                        needle_tip_coords,
+                                                        save_name=f'needle_tip_{image_count}_{current_depth_relative:.2f}')
+                image_count += 1
+        except KeyboardInterrupt:
+            break
         
     print("Reached target depth")
 
---
+++ robot_controller.py
@@ -0,0 +1,62 @@
+import rospy
+from geometry_msgs.msg import Vector3, Transform
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+import time
+
+
+class RobotController():
+    def __init__(self):
+        self.robot_ee_frame_sub = rospy.Subscriber("/eye_robot/FrameEE", Transform, self.update_pos_or)
+        self.pub_tip_vel = rospy.Publisher('/eyerobot2/desiredTipVelocities', Vector3, queue_size = 3)
+        self.pub_tip_vel_angular = rospy.Publisher('/eyerobot2/desiredTipVelocitiesAngular', Vector3, queue_size = 3)
+        rospy.sleep(0.5)
+        self.position = []
+        self.orientation = []
+        
+    def update_pos_or(self, data):
+        x = data.translation.x
+        y = data.translation.y
+        z = data.translation.z
+        rx = data.rotation.x
+        ry = data.rotation.y
+        rz = data.rotation.z
+        rw = data.rotation.w
+        self.position = np.array([x, y, z])
+        self.orientation = np.array([rx, ry, rz, rw])
+
+    def move_forward_needle_axis(self, kp_linear_vel=2, linear_vel=0.1, duration_sec=1):
+        current_quat = self.orientation
+        r_current = R.from_quat(current_quat)
+        rotation_matrix_current = r_current.as_matrix()
+        moving_direction = np.matmul(rotation_matrix_current, np.array((0, 0, -1)))
+        send_linear_velocity = moving_direction * kp_linear_vel * linear_vel
+
+        for _ in range(int(duration_sec / 0.1)):
+            self.pub_tip_vel.publish(send_linear_velocity[0], send_linear_velocity[1], send_linear_velocity[2])
+            rospy.sleep(0.1)
+        self.pub_tip_vel.publish(0, 0, 0)
+
+    def move_backward_needle_axis(self, kp_linear_vel=2, linear_vel=0.1, duration_sec=1):
+        current_quat = self.orientation
+        r_current = R.from_quat(current_quat)
+        rotation_matrix_current = r_current.as_matrix()
+        moving_direction = np.matmul(rotation_matrix_current, np.array((0, 0, 1)))
+        send_linear_velocity = moving_direction * kp_linear_vel * linear_vel
+
+        start = time.perf_counter()
+        for _ in range(int(duration_sec / 0.1)):
+            self.pub_tip_vel.publish(send_linear_velocity[0], send_linear_velocity[1], send_linear_velocity[2])
+            rospy.sleep(0.1)
+        end = time.perf_counter()
+        self.pub_tip_vel.publish(0, 0, 0)
+        print(f'took {end-start}')
+
+
+# if __name__ == '__main__':
+#     controller = RobotController()
+#     controller.move_forward_needle_axis(kp_linear_vel=2, linear_vel=0.1, duration_sec=2)
+    # controller.move_backward_needle_axis()
+
+    # controller.pub_tip_vel(0,0,0)

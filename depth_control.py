8be95bf037ea31d76d850d58a713291008b933d2
Demir Arikan <dmrarikan@gmail.com>
2024-07-12 21:29:19 -0400

    add logger class for debugging

--- depth_control.py
+++ depth_control.py
@@ -1,10 +1,15 @@
-# import leica_engine
 import oct_point_cloud
 from mock_leica import MockLeica
 from needle_seg_model import NeedleSegModel
-# import rospy
-# from robot_controller import RobotController
-import numpy as np
+from logger import Logger
+
+MOCK_MODE = True
+
+if not MOCK_MODE:
+    from leica_engine import LeicaEngine
+    from robot_controller import RobotController
+    import rospy
+
 
 target_depth_relative = 0.5
 current_depth_relative = 0.0
@@ -14,67 +19,57 @@ n_bscans = 8
 dims = (0.1, 4)
 
 save_point_cloud_image = True
-image_count = 0
 
 if __name__ == '__main__':
-    # rospy.init_node('depth_controller', anonymous=True)
-
-    # robot_controller = RobotController()
     seg_model = NeedleSegModel('weights/epoch_170_val_loss_0.3113_model_straight_needle_segmentation2d_dict.pth')
-    # leica_reader = leica_engine.LeicaEngine(ip_address="192.168.1.75",   
-    #                                n_bscans=n_bscans, 
-    #                                xd=dims[0], 
-    #                                yd=dims[1], 
-    #                                zd=3.379,
-    #                                )
-    leica_reader = MockLeica('/home/demir/Desktop/jhu_project/oct_scans/jun18/2.3')
-    print("Leica reader initialized")
+    
+    if MOCK_MODE:
+        leica_reader = MockLeica('/home/demir/Desktop/jhu_project/oct_scans/jun18/2.3')
+    else:
+        rospy.init_node('depth_controller', anonymous=True)
+        robot_controller = RobotController()
+        leica_reader = LeicaEngine(ip_address="192.168.1.75",   
+                                   n_bscans=n_bscans, 
+                                   xd=dims[0], 
+                                   yd=dims[1], 
+                                   zd=3.379,
+                                   )
+    logger = Logger()
 
     while current_depth_relative < target_depth_relative:
         try:
-            oct_volume, _ = leica_reader.__get_b_scans_volume__()
+            raw_oct_volume, _ = leica_reader.__get_b_scans_volume__()
 
-            oct_volume = seg_model.preprocess_volume(oct_volume, save_train_img=False)
-            seg_volume = seg_model.segment_volume(oct_volume, debug=True)
+            logger.log_volume(raw_oct_volume)
+
+            oct_volume = seg_model.preprocess_volume(raw_oct_volume, save_train_img=False)
+            seg_volume = seg_model.segment_volume(oct_volume, debug=False)
             seg_volume = seg_model.postprocess_volume(seg_volume)
 
+            logger.log_seg_results(oct_volume, seg_volume)
 
-            inpaint = oct_point_cloud.inpaint_layer(seg_volume)
-
+            # needle processing
             needle_point_cloud = oct_point_cloud.create_point_cloud_from_vol(seg_volume, seg_index=[1])
-
             _, ransac_inliers = oct_point_cloud.needle_cloud_line_ransac(needle_point_cloud)
             cleaned_needle = needle_point_cloud.select_by_index(ransac_inliers)
-            # cleaned_needle.transform(np.array([[1, 0, 0, 100],
-            #                                    [0, 1, 0, 0],
-            #                                    [0, 0, 1, 0],
-            #                                    [0, 0, 0, 1]]))
-            # oct_point_cloud.draw_geometries([needle_point_cloud, cleaned_needle])
-
-
-            # cylinder = oct_point_cloud.create_cylinder_pcd(height=500)
-            # cylinder = oct_point_cloud.register_using_ransac(needle_point_cloud, cylinder, voxel_size=0.8)
-            # # oct_point_cloud.draw_geometries([needle_point_cloud, cylinder])
-            # cleaned_needle = oct_point_cloud.outlier_detection_needle_estimate(needle_point_cloud, cylinder)
-            # # oct_point_cloud.draw_geometries([cleaned_needle, cylinder])
-
-
             needle_tip_coords = oct_point_cloud.find_lowest_point(cleaned_needle)
             
+            # ILM RPE layers processing
             ilm_depth_map = oct_point_cloud.get_depth_map(seg_volume, seg_index=2)
             rpe_depth_map = oct_point_cloud.get_depth_map(seg_volume, seg_index=3)
 
-            ilm_points, rpe_points = oct_point_cloud.inpaint_layers(ilm_depth_map, rpe_depth_map, debug=True)
+            ilm_points, rpe_points = oct_point_cloud.inpaint_layers(ilm_depth_map, rpe_depth_map, debug=False)
 
             ilm_tip_coords = ilm_points[(ilm_points[:, 0] == needle_tip_coords[0]) & 
                                         (ilm_points[:, 2] == needle_tip_coords[2])]
-            
             rpe_tip_coords = rpe_points[(rpe_points[:, 0] == needle_tip_coords[0]) & 
                                         (rpe_points[:, 2] == needle_tip_coords[2])]
             
             _, _, current_depth_relative = oct_point_cloud.calculate_needle_tip_depth(needle_tip_coords, 
                                                                             ilm_tip_coords[0], 
                                                                             rpe_tip_coords[0])
+            
+            logger.log_result_oct(needle_tip_coords, current_depth_relative)
             print(f"Current depth: {current_depth_relative}")
 
             if save_point_cloud_image:
@@ -82,18 +77,23 @@ if __name__ == '__main__':
                                                         ilm_points,
                                                         rpe_points,
                                                         needle_tip_coords,
-                                                        save_name=f'needle_tip_{image_count}_{current_depth_relative:.2f}')
-                image_count += 1
+                                                        save_path=logger.get_pcd_save_dir(),
+                                                        save_name=logger.get_pcd_save_name())
 
-            if (current_depth_relative >= 0 and 
-                current_depth_relative <= target_depth_relative+error_range and 
-                current_depth_relative >= target_depth_relative-error_range):
-                # robot_controller.stop()
-                break
-            else:
-                print('Current depth smaller than target, moving robot')
-                # robot_controller.move_forward_needle_axis(duration_sec=0.5)
-                # robot_controller.stop()
+            logger.increment_image_count()
+
+            if not MOCK_MODE:
+                # if (current_depth_relative >= 0 and 
+                #     current_depth_relative <= target_depth_relative+error_range and 
+                #     current_depth_relative >= target_depth_relative-error_range):
+                if abs(current_depth_relative - target_depth_relative) < error_range:
+                    robot_controller.stop()
+                    break
+                else:
+                    print('Current depth smaller than target, moving robot')
+                    robot_controller.move_forward_needle_axis(duration_sec=0.5)
+                    robot_controller.stop()
+
         except KeyboardInterrupt:
             break
         

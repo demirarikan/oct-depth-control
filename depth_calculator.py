6349f5560eb5789c632f26a541b6e7c49db3591c
Demir Arikan <dmrarikan@gmail.com>
2024-07-22 20:45:29 -0400

    changed depth controller to calculator, only calculates depth

---
+++ depth_calculator.py
@@ -0,0 +1,20 @@
+from oct_point_cloud import OctPointCloud
+
+
+class DepthCalculator:
+    def __init__(self, logger):
+        self.logger = logger
+
+    def calculate_depth(self, segmented_oct_volume, log_final_pcd=False):
+        oct_pcd = OctPointCloud(seg_volume=segmented_oct_volume)
+        needle_tip_coords = oct_pcd.find_needle_tip()
+        inpainted_ilm, inpainted_rpe = oct_pcd.inpaint_layers(debug=False)
+        _, _, current_depth_relative = oct_pcd.calculate_needle_tip_depth(
+            needle_tip_coords, inpainted_ilm, inpainted_rpe
+        )
+        if log_final_pcd:
+            self.logger.log_pcd(
+                oct_pcd.create_point_cloud_components(needle_tip_coords)
+            )
+        return current_depth_relative
+

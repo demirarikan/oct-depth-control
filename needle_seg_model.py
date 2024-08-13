fixed needle segmentation cropping images in the wrong axis, added model generation from dictionary

needle_seg_model.py
@@ -40,7 +40,7 @@ class NeedleSegModel:
         # oct_volume = np.rot90(oct_volume, axes=(1, 2))
         oct_volume = oct_volume.astype(np.float32)
         oct_volume = torch.tensor(oct_volume).to(self.device)
-        oct_volume = transforms.Pad((12, 0, 12, 0))(oct_volume)
+        # oct_volume = transforms.Pad((12, 0, 12, 0))(oct_volume)
         oct_volume = oct_volume.unsqueeze(1)
         return oct_volume
 
f851bf5437d0b4e3b4ae80dbf5e7771ca8b1351b
Demir Arikan <dmrarikan@gmail.com>
2024-10-28 14:48:20 -0400

    updated scatter plot line names

--- vis_z_motion.py
+++ vis_z_motion.py
@@ -3,7 +3,7 @@ import plotly.graph_objs as go
 import plotly.io as pio
 
 # Path to CSV file
-csv_path = "motion_median.csv"
+csv_path = "/home/demir/Desktop/oct14-breathing/100um-3.csv"
 
 # Initialize lists for data
 x = []
@@ -21,14 +21,14 @@ with open(csv_path, 'r') as csvfile:
             first_rob_pos = float(row[2])
             first_linear_stage_pos = float(row[7])
         x.append(float(row[8]))
-        robot_z_pos.append(-(float(row[2])-first_rob_pos))
+        robot_z_pos.append((float(row[2])-(first_rob_pos-0.5)))
         linear_stage_z_pos.append(float(row[7])-first_linear_stage_pos)
 
 
 
 # Create traces for the two plots
-trace1 = go.Scatter(x=x, y=robot_z_pos, mode='lines', name='Line 1')
-trace2 = go.Scatter(x=x, y=linear_stage_z_pos, mode='lines', name='Line 2')
+trace1 = go.Scatter(x=x, y=robot_z_pos, mode='lines', name='Robot Z pos')
+trace2 = go.Scatter(x=x, y=linear_stage_z_pos, mode='lines', name='Eye Z pos')
 
 # Create a figure with the two traces
 fig = go.Figure(data=[trace1, trace2])
